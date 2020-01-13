9483fb4ee8cc0be9ceb9c5f245a3bb5bd50052d7
Hemanth Krishna <31009634+darthbenro008@users.noreply.github.com>
2020-01-14 00:56:15 +0530

    Nav Controller Added

---
+++ app/src/main/java/com/benrostudios/studyweather/ui/weather/future/list/FutureListViewModel.kt
@@ -0,0 +1,7 @@
+package com.benrostudios.studyweather.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
